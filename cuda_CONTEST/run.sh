a6bae2474898784a94c168812a4b5c377e6b0ecd
RIccardoIzzo <izzo.riccardo38@gmail.com>
2022-06-26 20:09:14 +0200

    update run.sh

--- cuda_CONTEST/run.sh
+++ cuda_CONTEST/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Run vector sum;
-bin/b -d -c -n 100000000 -b vec -I 1 -i 30 -t 64;
-bin/b -d -c -n 100000000 -b vec -I 2 -i 30 -t 64;
+# bin/b -d -c -n 100000000 -b vec -I 1 -i 30 -t 64;
+# bin/b -d -c -n 100000000 -b vec -I 2 -i 30 -t 64;
 
 # Run matrix multiplication;
-bin/b -d -c -n 1000 -b mmul -I 1 -i 30 -t 8;
-bin/b -d -c -n 1000 -b mmul -I 2 -i 30 -t 8 -B 14;
+# bin/b -d -c -n 1000 -b mmul -I 1 -i 30 -t 8;
+# bin/b -d -c -n 1000 -b mmul -I 2 -i 30 -t 8 -B 14;
 
 # Run Personalized PageRank
-bin/b -d -c -b ppr -I 1 -i 100 -t 256 -g data/wikipedia-20070206.mtx;
+bin/b -d -c -b ppr -I 1 -i 100 -t 256 -e 1e-4 -g data/wikipedia-20070206.mtx;
